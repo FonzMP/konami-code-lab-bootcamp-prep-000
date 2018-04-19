400539b7942c0be80ecabad6966348919a916b8a
Alex Wilson <fonzmp@gmail.com>
2018-04-19 20:36:29 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0;
 
-const div = document.getElementById('mocha');
+const div = document.querySelector('#mocha');
 div.style.color = "green";
 
 function init(e) {
