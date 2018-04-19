80628de012577b80262b015ff11a44c9751c236a
Alex Wilson <fonzmp@gmail.com>
2018-04-19 21:28:23 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ let index = 0;
 
 function init(e) {
   // Write your JavaScript code inside the init() function
-  const key = e.which;
+  const key = parseInt(e.which);
   if (key === parseInt(code[index])) {
     index++;
     if (index === code.length) {
