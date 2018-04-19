af6e3de4e9e6772373b6db879b5f4131a4388d0c
Alex Wilson <fonzmp@gmail.com>
2018-04-19 20:52:26 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,10 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
+function init(e) {
   // Write your JavaScript code inside the init() function
+  const key = parseInt(e.detail || e.which);
+  for (let i = 0; i < code.length; i++)
+  if (key === alphabet[index]) {
+    i++
+  }
 }
