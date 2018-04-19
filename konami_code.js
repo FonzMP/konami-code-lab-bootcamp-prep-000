e2129c257fc0f46126f8e79aa69c2bd0dfc81aab
Alex Wilson <fonzmp@gmail.com>
2018-04-19 20:39:19 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0;
 
-document.body.addEventListener('keydown', init());
+
 
 function init(e) {
   // Write your JavaScript code inside the init() function
@@ -19,3 +19,5 @@ function init(e) {
     index = 0;
   }
 }
+
+document.body.addEventListener('keydown', init());
