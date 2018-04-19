c8aefdfb3c1df2507eb00e179202858078509aed
Alex Wilson <fonzmp@gmail.com>
2018-04-19 20:50:10 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,6 +2,11 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0;
 
+document.body.addEventListener('keydown', function(e){
+  const key = parseInt(e.detail || e.which);
+  return key
+})
+
 function init() {
   // Write your JavaScript code inside the init() function
   document.body.addEventListener('keydown', function(e) {
@@ -19,7 +24,4 @@ function init() {
   }
 }
 
-document.body.addEventListener('keydown', function(e){
-  const key = parseInt(e.detail || e.which);
-  return e
-})
+
