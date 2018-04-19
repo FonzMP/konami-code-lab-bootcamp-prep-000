d35fcd1391bc202caa914a0996f4a9175fc2650e
Alex Wilson <fonzmp@gmail.com>
2018-04-19 21:37:36 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,21 +2,22 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0;
 
-function init(e) {
-  // Write your JavaScript code inside the init() function
-  const key = parseInt(e.which);
-  if (key === parseInt(code[index])) {
-    index++;
-    if (index === code.length) {
-      alert("Hurray, you did it!");
-      index = 0;
-      return
-    }
-  } else {
-    alert('Code has been reset');
-    index = 0;
-  }
-}
-
 const bodyVar = document.body;
-bodyVar.addEventListener('keydown', init);
+
+function init() {
+  // Write your JavaScript code inside the init() function
+  bodyVar.addEventListener('keydown', function(e){
+    const key = parseInt(e.which);
+    if (key === parseInt(code[index])) {
+      index++;
+      if (index === code.length) {
+        alert("Hurray, you did it!");
+        index = 0;
+        return
+      }
+    } else {
+      alert('Code has been reset');
+      index = 0;
+    }
+  });
+}
