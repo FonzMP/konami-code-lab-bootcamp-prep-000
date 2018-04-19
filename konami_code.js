bbc2b62737d5df48512ed15ec4f250cd9da4c33c
Alex Wilson <fonzmp@gmail.com>
2018-04-19 20:35:36 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,6 +2,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0;
 
+const div = document.getElementById('mocha');
+div.style.color = "green";
+
 function init(e) {
   // Write your JavaScript code inside the init() function
   const key = parseInt(e.detail || e.which);
